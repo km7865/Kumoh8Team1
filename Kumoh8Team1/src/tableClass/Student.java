d91e863cba5592200ab33d8b2cdb089407fd568a
Jahwi-0413 <58069290+Jahwi-0413@users.noreply.github.com>
2019-12-03 20:21:42 +0900

    학생테이블에 보호자 정보, 학생 우편번호, 주야구분 삭제

--- Kumoh8Team1/src/tableClass/Student.java
+++ Kumoh8Team1/src/tableClass/Student.java
@@ -11,15 +11,8 @@ public class Student
 	private String departmentName;		//학과명
 	private double grade;				//학년
 	private String major;				//전공구분
-	private String DayOrNight;			//주야구분
-	private String studentPostcode;		//학생우편번호
 	private String studentAddress;		//학생주소
 	private String studentPhoneNumber;	//학생전화번호
-	private String parentsName;			//보호자이름
-	private String parentsPostcode;		//보호자우편번호	
-	private String parentsAddress;		//보호자주소
-	private String parentsPhoneNumber;	//보호자전화번호
-	private String parentsRelation;		//보호자와의 관계
 
 	public Student(String university, String studentId, String name, String registrationNumber)
 	{
@@ -28,10 +21,7 @@ public class Student
 		this.name = name;
 		this.registrationNumber = registrationNumber;
 		gender = null; departmentCode = null; departmentName = null;
-		grade = 0; major = null; DayOrNight = null;
-		studentPostcode = null; studentAddress = null; studentPhoneNumber = null;
-		parentsName = null; parentsAddress = null; parentsPhoneNumber = null;
-		parentsRelation = null;
+		grade = 0; major = null; studentAddress = null; studentPhoneNumber = null;
 	}
 
 	public Student (Student some)
@@ -42,10 +32,7 @@ public class Student
 		registrationNumber = some.getRegistrationNumber();	
 		gender = some.getGender(); departmentCode = some.getDepartmentCode(); 
 		departmentName = some.getDepartmentName(); grade = some.getGrade(); major = some.getMajor();
-		DayOrNight = some.getDayOrNight(); studentPostcode = some.getStudentPostCode(); 
-		studentAddress = some.getStudentAddress(); studentPhoneNumber = some.getStudentPhoneNumber();
-		parentsName = some.getParentsName(); parentsAddress = some.getParentAddress(); 
-		parentsPhoneNumber = some.getParentsPhoneNumber(); parentsRelation = some.getParentsRelation();	
+		studentAddress = some.getStudentAddress(); studentPhoneNumber = some.getStudentPhoneNumber();		
 	}
 	
 	public String getUniversity() {return university;}
@@ -57,15 +44,9 @@ public class Student
 	public String getDepartmentName() {return departmentName;}
 	public double getGrade() {return grade;}
 	public String getMajor() {return major;}
-	public String getDayOrNight() {return DayOrNight;}
-	public String getStudentPostCode() {return studentPostcode;}
 	public String getStudentAddress() {return studentAddress;}
 	public String getStudentPhoneNumber() {return studentPhoneNumber;}
-	public String getParentsRelation() {return parentsRelation;}
-	public String getParentsName() {return parentsName;}
-	public String getParentsPhoneNumber() {return parentsPhoneNumber;}
-	public String getParentAddress() {return parentsAddress;}
-	
+
 	public void setUniversity(String university) {this.university = university;}
 	public void setStudentId(String studentId) {this.studentId = studentId;}
 	public void setName(String name) {this.name = name;}
@@ -74,12 +55,6 @@ public class Student
 	public void setDepartmenCode(String departmentCode) {this.departmentCode = departmentCode;}
 	public void serDepartmentName(String departmentName) {this.departmentName = departmentName;}
 	public void setGrade(double grade) {this.grade = grade;}
-	public void setDayOrNight(String DayOrNight) {this.DayOrNight = DayOrNight;}
-	public void setStudentPostcode(String studentPostCode) {this.studentPostcode = studentPostCode;}
 	public void setStudentAddress(String studentAddress) {this.studentAddress = studentAddress;}
 	public void setStudentPhoneNumber(String studentPhoneNumber) {this.studentPhoneNumber = studentPhoneNumber;}
-	public void setParentsName(String parentsName) {this.parentsName = parentsName;}
-	public void setParentsAddress(String parentsAddress) {this.parentsAddress = parentsAddress;}
-	public void setParentsPhoneNumber(String parenstPhoneNumber) {this.parentsPhoneNumber = parentsPhoneNumber;}
-	public void setParentsRelation(String parentsRelation) {this.parentsRelation = parentsRelation;}
 }
