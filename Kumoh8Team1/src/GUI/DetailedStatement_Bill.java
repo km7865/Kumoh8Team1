aa5783e37572aff9e6b9cc667bbc20a20cccd23b
km7865 <51434817+km7865@users.noreply.github.com>
2019-12-10 14:06:46 +0900

    입사신청내역 및 고지서출력 생성자 수정

--- Kumoh8Team1/src/GUI/DetailedStatement_Bill.java
+++ Kumoh8Team1/src/GUI/DetailedStatement_Bill.java
@@ -1,5 +1,4 @@
 // 입사신청 내역조회 및 고지서 출력
-
 package GUI;
 
 import java.awt.BorderLayout;
@@ -8,6 +7,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import Network.Protocol;
+import tableClass.dormitoryApplication;
+
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.io.InputStream;
@@ -16,21 +19,18 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.awt.event.ActionEvent;
 
-import Network.Protocol;
-import tableClass.*;
-
 public class DetailedStatement_Bill extends JFrame {
 	private JPanel contentPane;
-	
+
 	private static Protocol p;
-	private static ObjectOutputStream writer;
+	private static ObjectOutputStream writer; 
 	private static ObjectInputStream reader;
 
-	public DetailedStatement_Bill(Protocol p_t, ObjectOutputStream oos, ObjectInputStream ois) {
+	public DetailedStatement_Bill(Protocol p_t, ObjectOutputStream writer_t, ObjectInputStream reader_t) {
 		p = p_t;
-		writer = oos;
-		reader = ois;
-		
+		writer = writer_t;
+		reader = reader_t;
+		this.setVisible(true);
 		this.setResizable(false); // 최대화 단추 없애기
 		setTitle("입사신청 내역조회 및 고지서 출력");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -43,7 +43,7 @@ public class DetailedStatement_Bill extends JFrame {
 		JButton button = new JButton("입사신청 내역조회");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//new Join_Application_DetailedStatement(p); // 수정 필요!!
+				new Join_Application_DetailedStatement(p_t,writer, reader); // 수정 필요!!
 			}
 		});
 		button.setBounds(30, 40, 140, 40);
@@ -52,7 +52,7 @@ public class DetailedStatement_Bill extends JFrame {
 		JButton button_1 = new JButton("고지서 출력");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new Bill(p, writer, reader);
+				new Bill(p_t, writer, reader);
 			}
 		});
 		button_1.setBounds(195, 40, 130, 40);
